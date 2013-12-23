8994432174a2f0c0ac3e4db9d1e623650d425cb6
Anton Agestam <msn@antonagestam.se>
2013-12-23 18:47:09 +0100

    Update runtests.py

--- runtests.py
+++ runtests.py
@@ -10,26 +10,11 @@ from django.core.management import call_command
 
 
 def main():
-    """
-    The entry point for the script. This script is fairly basic. Here is a
-    quick example of how to use it::
-
-        app_test_runner.py [path-to-app]
-
-    You must have Django on the PYTHONPATH prior to running this script. This
-    script basically will bootstrap a Django environment for you.
-
-    By default this script with use SQLite and an in-memory database. If you
-    are using Python 2.5 it will just work out of the box for you.
-
-    TODO: show more options here.
-    """
     parser = OptionParser()
     parser.add_option("--DATABASE_ENGINE", dest="DATABASE_ENGINE", default="sqlite3")
     parser.add_option("--DATABASE_NAME", dest="DATABASE_NAME", default="")
     parser.add_option("--DATABASE_USER", dest="DATABASE_USER", default="")
     parser.add_option("--DATABASE_PASSWORD", dest="DATABASE_PASSWORD", default="")
-    parser.add_option("--SITE_ID", dest="SITE_ID", type="int", default=1)
 
     options, args = parser.parse_args()
 
@@ -37,14 +22,8 @@ def main():
     app_path = 'access_manager'
     parent_dir, app_name = os.path.split(app_path)
     sys.path.insert(0, parent_dir)
-    print app_name
 
     settings.configure(**{
-        "PAYPAL_APPLICATION_ID": 'fake',
-        "PAYPAL_USERID": 'fake',
-        "PAYPAL_PASSWORD": 'fake',
-        "PAYPAL_SIGNATURE": 'test',
-        "PAYPAL_EMAIL": "fake@fake.com",
         "DATABASES": {
             'default': {
                 "ENGINE": 'django.db.backends.%s' % options.DATABASE_ENGINE,
